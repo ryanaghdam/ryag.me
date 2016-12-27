1cce3c61cf65e74748ff29c490535cda09c84ee9
Ryan Aghdam <ryan@ryanaghdam.com>
2016-12-27 15:15:44 -0500

    Short links in reverse chronological order

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,7 +5,7 @@ class LinksController < ApplicationController
   # GET /links
   # GET /links.json
   def index
-    @links = Link.all
+    @links = Link.order(updated_at: :desc)
   end
 
   # GET /links/1
