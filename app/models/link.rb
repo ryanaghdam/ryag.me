fe71d59d895113d92b68d44157c1feed307ef99c
Ryan Aghdam <ryan@ryanaghdam.com>
2016-12-26 20:51:02 -0500

    Add link to short url in interface

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,8 @@
 class Link < ApplicationRecord
   validates :alias, :target_url, :presence => true
   validates :alias, :uniqueness => true
+
+  def full_short_url
+    "http://ryag.me/#{self.alias}"
+  end
 end
